c9a4a5603ef02fa584daf224950de0e46a77074b
m3dry <m3dry@tutamail.com>
2023-04-23 19:08:39 +0200

    feat: schedule

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
-use chrono::{Datelike, Duration, Local, NaiveDate, NaiveTime};
+use chrono::{Datelike, Days, Duration, Local, NaiveDate, NaiveTime};
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DurationSeconds};
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd)]
+macro_rules! day_creation {
+    () => {
+        Day {
+            static_tasks: vec![],
+            dynamic_tasks: vec![],
+        }
+    };
+}
+
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd, Clone, Copy)]
 pub enum PartOfDay {
     Morning,
     Noon,
@@ -35,19 +44,240 @@ pub struct Planner {
 }
 
 impl Planner {
-    pub fn get_freetime_current(&self) -> Vec<TimeRange> {
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+            days: [
+                day_creation!(),
+                day_creation!(),
+                day_creation!(),
+                day_creation!(),
+                day_creation!(),
+                day_creation!(),
+                day_creation!(),
+            ],
+            dynamic_tasks: vec![],
+        }
+    }
+
+    pub fn get_freetime_current(&self) -> Vec<Schedule> {
         self.current_day().get_freetime(&self.config)
     }
 
-    pub fn get_freetime_nth(&self, n: usize) -> Result<Vec<TimeRange>, String> {
+    pub fn get_schedule_with_dynamics(&self) -> (Vec<Schedule>, Vec<String>) {
+        self.get_schedule_with_dynamics_nth(current()).unwrap()
+    }
+
+    pub fn get_freetime_nth(&self, n: usize) -> Result<Vec<Schedule>, String> {
         if n > 6 {
             return Err(format!("Expected n <= 6, n is {n}"));
         }
         Ok(self.days[n].get_freetime(&self.config))
     }
 
-    fn current_day(&self) -> &Day {
-        &self.days[Local::now().weekday().num_days_from_monday() as usize]
+    pub fn get_schedule_with_dynamics_nth(
+        &self,
+        n: usize,
+    ) -> Result<(Vec<Schedule>, Vec<String>), String> {
+        let mut freetime = self.get_freetime_nth(n)?;
+        let dynamic_tasks = self
+            .current_day()
+            .dynamic_tasks
+            .iter()
+            .map(|i| &self.dynamic_tasks[*i])
+            .collect::<Vec<&DynamicTask>>();
+        let mut errors = vec![];
+
+        for task in dynamic_tasks {
+            match task {
+                DynamicTask::Fixed {
+                    task: StaticTask { time, .. },
+                    ..
+                } => {
+                    for i in 0..freetime.len() {
+                        if let Schedule::Free(range) = &freetime[i] {
+                            if time.subset(range) {
+                                let first = if time.start != range.start {
+                                    Some(TimeRange::new(range.start, time.start))
+                                } else {
+                                    None
+                                };
+                                let second = if time.end != range.end {
+                                    Some(TimeRange::new(time.end, range.end))
+                                } else {
+                                    None
+                                };
+
+                                freetime[i] = Schedule::DynamicTask(task);
+                                if let Some(range) = first {
+                                    freetime.insert(i, Schedule::Free(range))
+                                }
+                                if let Some(range) = second {
+                                    if freetime.len() > i + 2 {
+                                        freetime.insert(i + 2, Schedule::Free(range))
+                                    } else {
+                                        freetime.push(Schedule::Free(range))
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+                DynamicTask::Flexible {
+                    length, can_split, ..
+                } => {
+                    if *can_split {
+                        let duration = freetime
+                            .iter()
+                            .enumerate()
+                            .filter_map(|(i, schedule)| {
+                                if let Schedule::Free(range) = schedule {
+                                    Some((i, range))
+                                } else {
+                                    None
+                                }
+                            })
+                            .fold((vec![], Duration::seconds(0)), |mut duration, range| {
+                                duration.0.push(range.0);
+                                (duration.0, duration.1 + range.1.to_duration())
+                            });
+                        if duration.1 >= *length {
+                            let mut length = length.to_owned();
+                            let mut part = 1;
+
+                            for i in duration.0 {
+                                if let Schedule::Free(range) = &freetime[i] {
+                                    let free_range = *range;
+                                    let duration = range.to_duration();
+                                    if duration == length {
+                                        freetime[i] = Schedule::DynamicPart(
+                                            task.fixed_split(&length, part).unwrap(),
+                                        );
+                                        break;
+                                    } else if duration > length {
+                                        freetime[i] = Schedule::DynamicPart(
+                                            task.fixed_split(&length, part).unwrap(),
+                                        );
+                                        if freetime.len() > i + 2 {
+                                            freetime.insert(
+                                                i + 2,
+                                                Schedule::Free(TimeRange::new(
+                                                    free_range.start + length,
+                                                    free_range.end,
+                                                )),
+                                            )
+                                        } else {
+                                            freetime.push(Schedule::Free(TimeRange::new(
+                                                free_range.start + length,
+                                                free_range.end,
+                                            )))
+                                        }
+                                        break;
+                                    } else {
+                                        freetime[i] = Schedule::DynamicPart(
+                                            task.fixed_split(&duration, part).unwrap(),
+                                        );
+                                        length = length - duration;
+                                        part += 1;
+                                    }
+                                } else {
+                                    panic!("this shouldn't happen")
+                                }
+                            }
+                        } else {
+                            errors.push(format!(
+                                "There isn't enough freetime for {task:#?}; total freetime {}",
+                                duration.1.num_seconds()
+                            ))
+                        }
+
+                        continue;
+                    }
+
+                    for i in 0..freetime.len() {
+                        if let Schedule::Free(range) = &freetime[i] {
+                            let duration = range.to_duration();
+                            if duration >= *length {
+                                if duration != *length {
+                                    if freetime.len() > i + 2 {
+                                        freetime.insert(
+                                            i + 2,
+                                            Schedule::Free(TimeRange::new(
+                                                range.start + *length,
+                                                range.end,
+                                            )),
+                                        )
+                                    } else {
+                                        freetime.push(Schedule::Free(TimeRange::new(
+                                            range.start + *length,
+                                            range.end,
+                                        )))
+                                    }
+                                }
+
+                                freetime[i] = Schedule::DynamicTask(task);
+                                break;
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        Ok((freetime, errors))
+    }
+
+    pub fn current_day(&self) -> &Day {
+        &self.days[current()]
+    }
+
+    pub fn nth_day(&self, nth: usize) -> Result<&Day, String> {
+        if nth > 6 {
+            return Err(format!("Expected n <= 6, n is {nth}"));
+        }
+        Ok(&self.days[nth])
+    }
+
+    pub fn current_day_mut(&mut self) -> &mut Day {
+        &mut self.days[current()]
+    }
+
+    pub fn nth_day_mut(&mut self, nth: usize) -> Result<&mut Day, String> {
+        if nth > 6 {
+            return Err(format!("Expected n <= 6, n is {nth}"));
+        }
+        Ok(&mut self.days[nth])
+    }
+
+    pub fn add_dynamic(&mut self, task: DynamicTask) -> Result<(), String> {
+        if task.get_date() < &Local::now().date_naive() {
+            return Err(String::from("Task can't start in the past"));
+        }
+
+        self.dynamic_tasks.push(task);
+        self.dynamic_tasks.sort();
+        Ok(())
+    }
+
+    pub fn update_dynamics(&mut self) {
+        let mut current_date = Local::now().date_naive();
+
+        for n in current()..7 {
+            self.days[n].dynamic_tasks = self
+                .dynamic_tasks
+                .iter()
+                .enumerate()
+                .filter_map(|(i, task)| {
+                    if task.get_date() == &current_date {
+                        Some(i)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            current_date = current_date.checked_add_days(Days::new(1)).unwrap();
+        }
     }
 }
 
@@ -63,36 +293,53 @@ impl Day {
         self.static_tasks.sort();
     }
 
-    pub fn get_freetime(&self, config: &Config) -> Vec<TimeRange> {
+    pub fn get_freetime(&self, config: &Config) -> Vec<Schedule> {
         let times = self
             .static_tasks
             .iter()
-            .map(|task| &task.time)
-            .collect::<Vec<&TimeRange>>();
+            .map(|task| (&task.time, task))
+            .collect::<Vec<(&TimeRange, &StaticTask)>>();
         let times_len = times.len();
         let mut free = vec![];
 
         for i in 0..times_len {
             if i == 0 {
-                if config.wake_time != times[i].start {
-                    free.push(TimeRange::new(config.wake_time, times[i].start))
+                if config.wake_time != times[i].0.start {
+                    free.push(Schedule::Free(TimeRange::new(
+                        config.wake_time,
+                        times[i].0.start,
+                    )))
                 }
+                free.push(Schedule::Static(times[i].1));
                 continue;
             }
 
-            if times[i - 1].end != times[i].start {
-                free.push(TimeRange::new(times[i - 1].end, times[i].start))
+            if times[i - 1].0.end != times[i].0.start {
+                free.push(Schedule::Free(TimeRange::new(
+                    times[i - 1].0.end,
+                    times[i].0.start,
+                )));
             }
+            free.push(Schedule::Static(times[i].1));
 
-            if i == times_len - 1 {
-                if times[i].end != config.bed_time {
-                    free.push(TimeRange::new(times[i].end, config.bed_time))
-                }
+            if i == times_len - 1 && times[i].0.end != config.bed_time {
+                free.push(Schedule::Free(TimeRange::new(
+                    times[i].0.end,
+                    config.bed_time,
+                )))
             }
         }
 
         free
     }
+}
+
+#[derive(Debug)]
+pub enum Schedule<'a> {
+    Static(&'a StaticTask),
+    DynamicTask(&'a DynamicTask),
+    DynamicPart(DynamicTask),
+    Free(TimeRange),
 }
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
@@ -126,7 +373,7 @@ impl Ord for StaticTask {
 
 impl PartialOrd for StaticTask {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(&other))
+        Some(self.cmp(other))
     }
 }
 
@@ -145,6 +392,7 @@ pub enum DynamicTask {
         #[serde_as(as = "DurationSeconds<i64>")]
         length: Duration,
         around: PartOfDay,
+        can_split: bool,
         priority: i32,
     },
     Fixed {
@@ -155,12 +403,19 @@ pub enum DynamicTask {
 }
 
 impl DynamicTask {
-    pub fn new_flexible(task: Task, date: NaiveDate, length: Duration, around: PartOfDay) -> Self {
+    pub fn new_flexible(
+        task: Task,
+        date: NaiveDate,
+        length: Duration,
+        around: PartOfDay,
+        can_split: bool,
+    ) -> Self {
         Self::Flexible {
             task,
             date,
             length,
             around,
+            can_split,
             priority: 0,
         }
     }
@@ -180,11 +435,50 @@ impl DynamicTask {
         }
         self
     }
+
+    fn get_date(&self) -> &NaiveDate {
+        match self {
+            DynamicTask::Flexible { date, .. } | DynamicTask::Fixed { date, .. } => date,
+        }
+    }
+
+    fn fixed_split(&self, length: &Duration, nth: usize) -> Result<Self, String> {
+        match self {
+            Self::Flexible {
+                task: Task {
+                    name, description, ..
+                },
+                date,
+                around,
+                can_split,
+                priority,
+                ..
+            } => Ok(Self::Flexible {
+                task: Task::new(format!("{name}-{nth}"), description.to_owned()),
+                date: date.to_owned(),
+                length: length.to_owned(),
+                around: *around,
+                can_split: *can_split,
+                priority: *priority,
+            }),
+            _ => Err(String::from("Expected a flexible dynamic task")),
+        }
+    }
 }
 
 impl Ord for DynamicTask {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         match (self, other) {
+            (Self::Flexible { date: date1, .. }, Self::Fixed { date: date2, .. })
+                if date1 > date2 =>
+            {
+                std::cmp::Ordering::Less
+            }
+            (Self::Fixed { date: date1, .. }, Self::Flexible { date: date2, .. })
+                if date1 > date2 =>
+            {
+                std::cmp::Ordering::Greater
+            }
             (Self::Flexible { .. }, Self::Fixed { .. }) => std::cmp::Ordering::Greater,
             (Self::Fixed { .. }, Self::Flexible { .. }) => std::cmp::Ordering::Less,
             (
@@ -219,11 +513,11 @@ impl Ord for DynamicTask {
 
 impl PartialOrd for DynamicTask {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(&other))
+        Some(self.cmp(other))
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Clone, Copy)]
 pub struct TimeRange {
     start: NaiveTime,
     end: NaiveTime,
@@ -238,4 +532,17 @@ impl TimeRange {
         (other.start >= self.start && other.start < self.end)
             || (other.end > self.start && other.end <= self.end)
     }
+
+    pub fn subset(&self, other: &Self) -> bool {
+        (self.start >= other.start && other.start < self.end)
+            && (self.end > other.start && other.end >= self.end)
+    }
+
+    pub fn to_duration(&self) -> Duration {
+        self.end.signed_duration_since(self.start)
+    }
+}
+
+fn current() -> usize {
+    Local::now().weekday().num_days_from_monday() as usize
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,79 @@
-use chrono::NaiveTime;
-use lunite::{Config, Day, PartOfDay, StaticTask, Task, TimeRange};
+use chrono::{Duration, NaiveDate, NaiveTime};
+use lunite::{Config, DynamicTask, PartOfDay, Planner, StaticTask, Task, TimeRange};
 
 fn main() {
-    let mut day: Day =
-        serde_json::from_str(r#"{ "static_tasks": [], "dynamic_tasks": [] }"#).unwrap();
-    let config = &Config::new(NaiveTime::from_hms(8, 0, 0), NaiveTime::from_hms(21, 0, 0));
+    let mut planner = Planner::new(Config::new(
+        NaiveTime::from_hms(8, 0, 0),
+        NaiveTime::from_hms(21, 0, 0),
+    ));
     let task1 = StaticTask::new(
         Task::new(format!("task1"), format!("")),
         TimeRange::new(NaiveTime::from_hms(8, 0, 0), NaiveTime::from_hms(10, 0, 0)),
     );
     let task2 = StaticTask::new(
         Task::new(format!("task2"), format!("")),
-        TimeRange::new(NaiveTime::from_hms(11, 0, 0), NaiveTime::from_hms(12, 0, 0)),
+        TimeRange::new(NaiveTime::from_hms(10, 0, 0), NaiveTime::from_hms(12, 0, 0)),
     );
     let task3 = StaticTask::new(
         Task::new(format!("task3"), format!("")),
-        TimeRange::new(NaiveTime::from_hms(13, 0, 0), NaiveTime::from_hms(17, 0, 0)),
+        TimeRange::new(NaiveTime::from_hms(13, 0, 0), NaiveTime::from_hms(14, 0, 0)),
     );
 
-    day.add_static(task1);
-    day.add_static(task2);
-    day.add_static(task3);
+    planner.current_day_mut().add_static(task1);
+    planner.current_day_mut().add_static(task2);
+    planner.current_day_mut().add_static(task3);
 
-    println!("{:#?}", day.get_freetime(config));
+    let dynamic_tasks = vec![
+        DynamicTask::new_flexible(
+            Task::new("task5".to_string(), "".to_string()),
+            NaiveDate::from_ymd_opt(2023, 4, 23).unwrap(),
+            Duration::minutes(120),
+            PartOfDay::Night,
+            false,
+        ),
+        DynamicTask::new_flexible(
+            Task::new("task6".to_string(), "".to_string()),
+            NaiveDate::from_ymd_opt(2023, 4, 23).unwrap(),
+            Duration::minutes(120),
+            PartOfDay::Morning,
+            true,
+        ),
+        DynamicTask::new_fixed(
+            StaticTask::new(
+                Task::new("task4".to_string(), "".to_string()),
+                TimeRange::new(
+                    NaiveTime::from_hms_opt(14, 0, 0).unwrap(),
+                    NaiveTime::from_hms_opt(16, 0, 0).unwrap(),
+                ),
+            ),
+            NaiveDate::from_ymd_opt(2023, 4, 23).unwrap(),
+        ),
+        // DynamicTask::new_fixed(
+        //     StaticTask::new(
+        //         Task::new("task5".to_string(), "".to_string()),
+        //         TimeRange::new(
+        //             NaiveTime::from_hms_opt(14, 0, 0).unwrap(),
+        //             NaiveTime::from_hms_opt(21, 0, 0).unwrap(),
+        //         ),
+        //     ),
+        //     NaiveDate::from_ymd_opt(2023, 4, 23).unwrap(),
+        // ),
+        DynamicTask::new_fixed(
+            StaticTask::new(
+                Task::new("task4".to_string(), "".to_string()),
+                TimeRange::new(
+                    NaiveTime::from_hms_opt(12, 30, 0).unwrap(),
+                    NaiveTime::from_hms_opt(12, 45, 0).unwrap(),
+                ),
+            ),
+            NaiveDate::from_ymd_opt(2023, 4, 23).unwrap(),
+        ),
+    ];
+
+    for task in dynamic_tasks {
+        planner.add_dynamic(task).unwrap();
+    }
+
+    planner.update_dynamics();
+    println!("{:#?}", planner.get_schedule_with_dynamics());
 }
